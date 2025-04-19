e1f17e6b8823052460f7d162b5ea0cb8f10a007d
FranklinCN <thz15916108729@163.com>
2025-04-19 13:51:00 +0800

    INDEX FIX

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_bp
 from dailysentence import daily_sentence_bp
 from news import news_bp
 from flask_cors import CORS
 from flask_sslify import SSLify
 
-def display_banner():
-    print("███████╗████████╗ █████╗ ██████╗ ███████╗ █████╗ ██████╗ ██╗")
-    print("██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██║")
-    print("███████╗   ██║   ███████║██████╔╝███████╗███████║██████╔╝██║")
-    print("╚════██║   ██║   ██╔══██║██╔══██╗╚════██║██╔══██║██╔═══╝ ██║")
-    print("███████║   ██║   ██║  ██║██║  ██║███████║██║  ██║██║     ██║")
-    print("╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝")
-    print("                                                            ")
-    print("GitHub: https://github.com/Aurora-Studio-Dev/Open.StarsAPI")
-    print(" ")
-
 app = Flask(__name__)
 CORS(app)
 sslify = SSLify(app)
 
-# 注册蓝图
 app.register_blueprint(weather_bp, url_prefix='/')
 app.register_blueprint(daily_sentence_bp, url_prefix='/')
 app.register_blueprint(news_bp, url_prefix='/')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    display_banner()
     app.run(host='0.0.0.0', port=5000, debug=True)
